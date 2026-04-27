90def9cd18d4810f7c0e5457eb5f190c14cd818b
Gaurav <rajputgaurav7455@gmail.com>
2026-04-27 21:12:36 +0530

    Added duplicate and age validation

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -2,18 +2,50 @@ const express = require("express");
 const router = express.Router();
 const Student = require("../models/Student");
 
-// Add Student
+
+// Add Student (with no duplicate roll no is allowed)
 router.post("/students", async (req, res) => {
   try {
-    const newStudent = new Student(req.body);
-    const savedStudent = await newStudent.save();
-    res.status(201).json(savedStudent);
+    const { name, rollNumber, class: studentClass, section, age } = req.body;
+
+    //  age must be positive
+    if (age <= 0) {
+      return res.status(400).json({
+        message: "Age must be positive "
+      });
+    }
+
+    // Check duplicate roll number
+    const existingStudent = await Student.findOne({ rollNumber });
+
+    if (existingStudent) {
+      return res.status(400).json({
+        message: "Roll number already exists"
+      });
+    }
+
+    // Create new student
+    const newStudent = new Student({
+      name,
+      rollNumber,
+      class: studentClass,
+      section,
+      age
+    });
+
+    await newStudent.save();
+
+    res.status(201).json(newStudent);
+
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(500).json({
+      error: error.message
+    });
   }
 });
 
-// Get All Students
+
+//  All Students
 router.get("/students", async (req, res) => {
   try {
     const students = await Student.find();
@@ -25,7 +57,7 @@ router.get("/students", async (req, res) => {
 
 module.exports = router;
 
-// Delete Student
+// for Delete Student
 router.delete("/students/:id", async (req, res) => {
   try {
     const deletedStudent = await Student.findByIdAndDelete(req.params.id);
@@ -41,7 +73,7 @@ router.delete("/students/:id", async (req, res) => {
   }
 });
 
-// Update Student
+// for Update Student
 router.put("/students/:id", async (req, res) => {
   try {
     const updatedStudent = await Student.findByIdAndUpdate(
@@ -74,5 +106,42 @@ router.get("/students/:id", async (req, res) => {
 
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+// Search Student by Name
+router.get("/students/search/:name", async (req, res) => {
+  try {
+    const students = await Student.find({
+      name: { $regex: req.params.name, $options: "i" }
+    });
+
+    res.status(200).json(students);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Search Student by Roll Number
+router.get("/students/rollNumber/:rollNumber", async (req, res) => {
+  try {
+    const student = await Student.findOne({
+      rollNumber: req.params.rollNumber
+    });
+
+    if (!student) {
+      return res.status(404).json({
+        message: "Student not found"
+      });
+    }
+
+    res.status(200).json(student);
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
   }
 });
